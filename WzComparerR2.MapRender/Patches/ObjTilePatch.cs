3675d29319a88593dc00fa9bd0ad067bca09d801
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2022-01-14 19:16:12 +0800

    Add files via upload

---
+++ WzComparerR2.MapRender/Patches/ObjTilePatch.cs
@@ -0,0 +1,18 @@
+﻿#if MapRenderV1
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace WzComparerR2.MapRender.Patches
+{
+    public class ObjTilePatch : RenderPatch
+    {
+        public override void Update(GameTime gameTime, RenderEnv env)
+        {
+            base.Update(gameTime, env);
+            this.RenderArgs.DisplayRectangle = Rectangle.Empty;
+        }
+    }
+}
+#endif
