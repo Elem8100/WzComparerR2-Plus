62bdac049c88138c24a8617d0dda4a2a5b1bdb9f
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2022-01-14 19:17:41 +0800

    Add files via upload

---
+++ WzComparerR2.MapRender/Patches2/LifeItem.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WzComparerR2.WzLib;
+
+namespace WzComparerR2.MapRender.Patches2
+{
+    public class LifeItem : SceneItem
+    {
+        public int ID { get; set; }
+        public LifeType Type { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int MobTime { get; set; }
+        public bool Flip { get; set; }
+        public bool Hide { get; set; }
+        public int Fh { get; set; }
+        public int Cy { get; set; }
+        public int Rx0 { get; set; }
+        public int Rx1 { get; set; }
+        public List<Tuple<int, int>> Quest { get; set; }
+        public List<Tuple<long, long>> Date { get; set; }
+
+        public ItemView View { get; set; }
+
+        public LifeInfo LifeInfo { get; set; }
+
+        public static LifeItem LoadFromNode(Wz_Node node)
+        {
+            var item = new LifeItem()
+            {
+                ID = node.Nodes["id"].GetValueEx(0),
+                Type = ParseLifeType(node.Nodes["type"].GetValueEx<string>(null)),
+                X = node.Nodes["x"].GetValueEx(0),
+                Y = node.Nodes["y"].GetValueEx(0),
+                MobTime = node.Nodes["mobTime"].GetValueEx(0),
+                Flip = node.Nodes["f"].GetValueEx(false),
+                Hide = node.Nodes["hide"].GetValueEx(false),
+                Fh = node.Nodes["fh"].GetValueEx(0),
+                Cy = node.Nodes["cy"].GetValueEx(0),
+                Rx0 = node.Nodes["rx0"].GetValueEx(0),
+                Rx1 = node.Nodes["rx1"].GetValueEx(0)
+            };
+            item.Quest = new List<Tuple<int, int>>();
+            item.Date = new List<Tuple<long, long>>();
+            if (item.Type == LifeType.Npc)
+            {
+                string path = $@"Npc\{item.ID:D7}.img";
+                var npcNode = PluginBase.PluginManager.FindWz(path);
+
+                int? npcLink = npcNode?.FindNodeByPath(@"info\link").GetValueEx<int>();
+                if (npcLink != null)
+                {
+                    path = $@"Npc\{npcLink.Value:D7}.img";
+                    npcNode = PluginBase.PluginManager.FindWz(path);
+                }
+
+                if (npcNode != null)
+                {
+                    foreach (Wz_Node conditionNode in npcNode.Nodes.Where(n => n.Text.StartsWith("condition")))
+                    {
+                        foreach (Wz_Node questNode in conditionNode.Nodes.Where(n => n.Text.All(char.IsDigit)))
+                        {
+                            item.Quest.Add(Tuple.Create(int.Parse(questNode.Text), Convert.ToInt32(questNode.Value)));
+                        }
+                        if (conditionNode.Nodes["dateStart"] != null || conditionNode.Nodes["dateEnd"] != null)
+                        {
+                            item.Date.Add(Tuple.Create(conditionNode.Nodes["dateStart"].GetValueEx<long>(0), conditionNode.Nodes["dateEnd"].GetValueEx<long>(0)));
+                        }
+                    }
+                }
+            }
+            return item;
+        }
+
+        private static LifeType ParseLifeType(string text)
+        {
+            switch (text)
+            {
+                case "m": return LifeType.Mob;
+                case "n": return LifeType.Npc;
+                default: return LifeType.Unknown;
+            }
+        }
+
+        public class ItemView
+        {
+            /// <summary>
+            /// 时间关联，单位为毫秒。
+            /// </summary>
+            public int Time { get; set; }
+
+            /// <summary>
+            /// 动画资源。
+            /// </summary>
+            public object Animator { get; set; }
+        }
+
+        public enum LifeType
+        {
+            Unknown = 0,
+            Mob = 1,
+            Npc = 2
+        }
+
+    }
+}
---
+++ WzComparerR2.MapRender/Patches2/ObjItem.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WzComparerR2.WzLib;
+
+namespace WzComparerR2.MapRender.Patches2
+{
+    public class ObjItem : SceneItem
+    {
+        public string OS { get; set; }
+        public string L0 { get; set; }
+        public string L1 { get; set; }
+        public string L2 { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+        public bool Flip { get; set; }
+        public string Tags { get; set; }
+        public List<Tuple<int, int>> Quest { get; set; }
+
+        public ItemView View { get; set; }
+
+        public static ObjItem LoadFromNode(Wz_Node node)
+        {
+            var item = new ObjItem()
+            {
+                OS = node.Nodes["oS"].GetValueEx<string>(null),
+                L0 = node.Nodes["l0"].GetValueEx<string>(null),
+                L1 = node.Nodes["l1"].GetValueEx<string>(null),
+                L2 = node.Nodes["l2"].GetValueEx<string>(null),
+
+                X = node.Nodes["x"].GetValueEx(0),
+                Y = node.Nodes["y"].GetValueEx(0),
+                Z = node.Nodes["z"].GetValueEx(0),
+
+                Flip = node.Nodes["f"].GetValueEx(false),
+                Tags = node.Nodes["tags"].GetValueEx<string>(null),
+            };
+            item.Quest = new List<Tuple<int, int>>();
+            if (item.Tags != null)
+            {
+                int questID;
+                if (int.TryParse(item.Tags, out questID) || (item.Tags.StartsWith("q") && int.TryParse(item.Tags.Substring(1), out questID)))
+                {
+                    item.Quest.Add(Tuple.Create(questID, 1));
+                }
+            }
+            if (node.Nodes["quest"] != null)
+            {
+                foreach (Wz_Node questNode in node.Nodes["quest"].Nodes)
+                {
+                    item.Quest.Add(Tuple.Create(int.Parse(questNode.Text), Convert.ToInt32(questNode.Value)));
+                }
+            }
+            return item;
+        }
+
+        public class ItemView
+        {
+            /// <summary>
+            /// 时间关联，单位为毫秒。
+            /// </summary>
+            public int Time { get; set; }
+
+            /// <summary>
+            /// 动画资源。
+            /// </summary>
+            public object Animator { get; set; }
+        }
+    }
+}
